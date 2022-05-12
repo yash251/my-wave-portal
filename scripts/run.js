f42e970e6dcb2a0373d3de2d2898cb79e47fd15a
Yash Jagtap <jagtapyash2512@gmail.com>
2022-05-12 21:43:53 +0530

    hre

--- scripts/run.js
+++ scripts/run.js
@@ -1,5 +1,5 @@
 const main = async () => {
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); //Hardhat Runtime Environment, or HRE
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
     console.log("Contract deployed to:", waveContract.address);
