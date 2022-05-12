db32073bb8169e5a9c33ad093603cc7918f52316
Yash Jagtap <jagtapyash2512@gmail.com>
2022-05-12 21:52:52 +0530

    basic fxn

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves; //state var automatically initialized to 0
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved", msg.sender);
+    }
+
+    function getTotalWaves() public view returns(uint256) {
+        console.log("We have %d total waves", totalWaves);
+        return totalWaves;
     }
 }
