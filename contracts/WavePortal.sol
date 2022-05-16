bf87510841ccbc2b26445d6b7e0ef723b9c0d0e8
Yash Jagtap <jagtapyash2512@gmail.com>
2022-05-16 12:02:37 +0530

    event & struct

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,22 +1,56 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
 contract WavePortal {
-    uint256 totalWaves; //state var automatically initialized to 0
+    uint256 totalWaves;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    /*
+     * I created a struct here named Wave.
+     * A struct is basically a custom datatype where we can customize what we want to hold inside it.
+     */
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+
+    /*
+     * I declare a variable waves that lets me store an array of structs.
+     * This is what lets me hold all the waves anyone ever sends to me!
+     */
+    Wave[] waves;
 
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("I AM SMART CONTRACT. POG.");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s has waved", msg.sender);
+        console.log("%s waved w/ message %s", msg.sender, _message);
+
+        /*
+         * This is where I actually store the wave data in the array.
+         */
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
-    function getTotalWaves() public view returns(uint256) {
-        console.log("We have %d total waves", totalWaves);
+    /*
+     * I added a function getAllWaves which will return the struct array, waves, to us.
+     * This will make it easy to retrieve the waves from our website!
+     */
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
