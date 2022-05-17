9d023db5f752284b74380de791015e04b4072892
Yash Jagtap <jagtapyash2512@gmail.com>
2022-05-17 22:32:34 +0530

    pick random no.

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,6 +6,10 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    /*
+     * We will be using this below to help generate a random number
+     */
+    uint256 private seed;
 
     event NewWave(address indexed from, uint256 timestamp, string message);
 
@@ -27,24 +31,42 @@ contract WavePortal {
 
     constructor() payable {
         console.log("I AM SMART CONTRACT. POG.");
+        /*
+         * Set the initial seed
+         */
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
-    totalWaves += 1;
-    console.log("%s has waved!", msg.sender);
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
 
-    waves.push(Wave(msg.sender, _message, block.timestamp));
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+        /*
+         * Generate a new seed for the next user that sends a wave
+         */
+        seed = (block.difficulty + block.timestamp + seed) % 100;
 
+        console.log("Random # generated: %d", seed);
+        /*
+         * Give a 50% chance that the user wins the prize.
+         */
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            /*
+             * The same code we had before to send the prize.
+             */
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
     emit NewWave(msg.sender, block.timestamp, _message);
-
-    uint256 prizeAmount = 0.0001 ether;
-    require(
-        prizeAmount <= address(this).balance,
-        "Trying to withdraw more money than the contract has."
-    );
-    (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-    require(success, "Failed to withdraw money from contract.");
-}
+    }
 
     /*
      * I added a function getAllWaves which will return the struct array, waves, to us.
